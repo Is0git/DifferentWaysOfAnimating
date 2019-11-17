34b333d33c7125e0639a11942dd8037c8cd86896
Danielius <dnlspasin@gmail.com>
2019-11-17 20:45:03 +0200

    animation drawable

--- app/src/main/java/com/example/animationspractise/MainActivity.kt
+++ app/src/main/java/com/example/animationspractise/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.animationspractise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var imageView: ImageView
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.start)
+        imageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener { (imageView.background as AnimationDrawable).start() }
+
     }
+
+
 }
